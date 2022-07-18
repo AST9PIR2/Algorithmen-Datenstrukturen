8e3ad09e9ff2782bfebf840653f5a0ca281822b5
AST9PIR2 <92747125+AST9PIR2@users.noreply.github.com>
2022-07-18 11:48:58 +0200

    Add files via upload

---
+++ Aufgabe_1/Ware.cpp
@@ -0,0 +1,95 @@
+//
+// Created by Pirmin on 27.05.2022.
+//
+
+#include "Ware.h"
+#include "exceptions.h"
+
+const std::string &Ware::getBezeichnung() const {
+    return bezeichnung;
+}
+
+void Ware::setBezeichnung(const std::string &bezeichnung) {
+    try{
+        for(int i = 0; i < bezeichnung.size(); i++){
+            if((bezeichnung[i] >= 'A' && bezeichnung[i] <= 'Z')||(bezeichnung[i] >= 'a' && bezeichnung[i] <= 'z')||
+            bezeichnung[i] == '_'){
+
+            }else {
+                throw ErrorWare("Setting description failed! Chosen upper/lower case letters A-Z and/or _ ");
+            }
+        }
+        Ware::bezeichnung = bezeichnung;
+    }catch (ErrorWare &e) {
+        std::cout << std::endl << "*** ErrorWare: " << e.what() << " *** " << std::endl << std::endl;
+    }
+}
+
+
+int Ware::getSeriennummer() const {
+            return seriennummer;
+}
+
+void Ware::setSeriennummer(int seriennummer) {
+    try{
+        if(seriennummer >= 0 && seriennummer <= 999999 ){
+            Ware::seriennummer = seriennummer;
+        }else {
+            throw ErrorWare("Setting serial number failed! Value out of range!");
+        }
+
+    }catch (ErrorWare &e) {
+        std::cout << std::endl << "*** ErrorWare: " << e.what() << " *** " << std::endl << std::endl;
+    }
+}
+
+double Ware::getGewicht() const {
+    return gewicht;
+}
+
+void Ware::setGewicht(double gewicht) {
+    try{
+        if(seriennummer >= 0 && seriennummer <= 999999 ){
+            Ware::gewicht = gewicht;
+        }else {
+            throw ErrorWare("Setting weight failed! Value out of range!");
+        }
+
+    }catch (ErrorWare &e) {
+        std::cout << std::endl << "*** ErrorWare: " << e.what() << " *** " << std::endl << std::endl;
+    }
+}
+
+double Ware::getEinkaufspreis() const {
+    return einkaufspreis;
+}
+
+void Ware::setEinkaufspreis(double einkaufspreis) {
+    try{
+        if(seriennummer >= 0 && seriennummer <= 999999 ){
+            Ware::einkaufspreis = einkaufspreis;
+        }else {
+            throw ErrorWare("Setting purchase price failed! Value out of range!");
+        }
+
+    }catch (ErrorWare &e) {
+        std::cout << std::endl << "*** ErrorWare: " << e.what() << " *** " << std::endl << std::endl;
+    }
+}
+
+double Ware::getVerkaufspreis() const {
+    return verkaufspreis;
+}
+
+void Ware::setVerkaufspreis(double verkaufspreis) {
+    try{
+        if(seriennummer >= 0 && seriennummer <= 999999 ){
+            Ware::verkaufspreis = verkaufspreis;
+        }else {
+            throw ErrorWare("Setting selling price failed! Value out of range!");
+        }
+
+    }catch (ErrorWare &e) {
+        std::cout << std::endl << "*** ErrorWare: " << e.what() << " *** " << std::endl << std::endl;
+    }
+}
